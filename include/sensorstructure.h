afa06363a485ea512e04e50766943b761ef4d091
shreyataneja <shreyataneja0301@gmail.com>
2019-12-09 22:27:21 -0500

    all header files

---
+++ include/sensorstructure.h
@@ -0,0 +1,11 @@
+
+#ifndef __SENSORSTRUCTURE_H__
+#define __SENSORSTRUCTURE_H__
+
+   typedef struct{
+   char time[50];
+	char sensor_name[50];
+    double value;
+	}sensor;  
+	
+#endif // __SENSORSTRUCTURE_H__
